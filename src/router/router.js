ac9f8c95822aa20587bc2eb63963f7809ef6669f
Gil Yosef <gily@gthsoftware.com>
2024-09-08 13:47:03 +0300

    Final

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TaskList from '../components/TaskList.vue';
-import TaskDetail from '../components/TaskDetail.vue';
 import TaskForm from '../components/TaskForm.vue';
 
 const routes = [
   { path: '/tasks', component: TaskList },
   { path: '/tasks/new', component: TaskForm },
-  { path: '/tasks/:id', component: TaskDetail },
   { path: '/tasks/:id/edit', component: TaskForm },
   { path: '/', redirect: '/tasks' } // Redirect to /tasks by default
 ];
